21bd319a0045d88a5808e8a230d15402b2bff9c2
George Delaportas <g.delaportas@gmail.com>
2018-02-21 03:31:59 +0200

    Finalize CSP & introduce Anti-Hijack extension
    This is the latest, stable and tested release for CSP and Anti-Hijacking
    control. This version enhances security, enables a robust mechanism to
    block attackers and protetcts your sessions from hijacking.

--- framework/misc/gates/content.php
+++ framework/misc/gates/content.php
@@ -13,7 +13,24 @@
     // Check for direct access
     if (!defined('micro_mvc'))
         exit();
-	
+
+	/*
+	UTIL::Load_Extension('anti_hijack', 'php');
+
+	function hijack_test($message)
+	{
+		if (!empty($message))
+			echo $message . '<br><br>';
+		else
+			echo '::All Good::<br><br>';
+	}
+
+	$attack_params = array('hijack_test', 'I have "HIJACKED" you!');
+	$normal_params = array('hijack_test');
+
+	Anti_Hijack($_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], time(), 10, $attack_params, $normal_params);
+	*/
+
     // Test - Fetch content based on ID (AJAX)
     if (!empty($_POST['content_id']))
     {
