d77f197f730a06451e3ed53194b285745338dc3a
George Delaportas <g.delaportas@gmail.com>
2022-11-17 00:30:27 +0200

    Update dates for certain files

--- NOTICE.php
+++ NOTICE.php
@@ -23,7 +23,7 @@
     *
     *
     *  @author George Delaportas (G0D) <g.delaportas@gmail.com>
-    *  @copyright  micro-MVC (2015 - 2021)
+    *  @copyright  micro-MVC (2015 - 2022)
     *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
     *  International Registered Trademark & Property of George Delaportas (G0D)
     */
