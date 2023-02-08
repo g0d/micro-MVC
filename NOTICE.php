6e82a9b4d11068f476f6c111b78f370218508f06
George Delaportas <g.delaportas@gmail.com>
2023-02-09 00:03:32 +0200

    Minor touches

--- NOTICE.php
+++ NOTICE.php
@@ -23,7 +23,7 @@
     *
     *
     *  @author George Delaportas (G0D) <g.delaportas@gmail.com>
-    *  @copyright  micro-MVC (2015 - 2022)
+    *  @copyright  micro-MVC (2015 - 2023)
     *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
     *  International Registered Trademark & Property of George Delaportas (G0D)
     */
